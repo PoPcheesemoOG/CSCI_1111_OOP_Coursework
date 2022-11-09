3e2b66a7e64cf642018bd47fb793ab897a1e2f77
Paul Helske <116106386+PoPcheesemoOG@users.noreply.github.com>
2022-11-09 15:51:28 -0700

    Update Exercise10_03.java

--- Unfinished_Code/Exercise_10_03/src/Exercise10_03.java
+++ Unfinished_Code/Exercise_10_03/src/Exercise10_03.java
@@ -1,4 +1,16 @@
-
+/* Name: Paul Helske
+ * Date: 11/09/2022
+ */
 public class Exercise10_03 {
+	
+	public static void main(String[] args) {
+		int a = 5;
+		MyInteger b = new MyInteger(a);
+		System.out.println("My Integer value is... " + b.getMyInteger());
+		System.out.println("My Integer is an Even..." + b.isEven());
+		System.out.println("My Integer is an Odd..." + b.isOdd());
+		System.out.println("My Integer is a Prime..." + b.isPrime());
+		
+	}
 
 }
